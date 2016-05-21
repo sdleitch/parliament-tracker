6d4644f1264c23698637f7ebb87a6ec7d61e061f
Scott Leitch <s.d.leitch@gmail.com>
2016-05-20 23:12:45 -0400

    added summary to bill. Will need to add scraper method

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506174326) do
+ActiveRecord::Schema.define(version: 20160520020053) do
 
   create_table "bills", force: :cascade do |t|
     t.date     "date_introduced"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160506174326) do
     t.string   "bill_type"
     t.integer  "parliament_number"
     t.integer  "member_id"
+    t.text     "summary"
   end
 
   add_index "bills", ["member_id"], name: "index_bills_on_member_id"
