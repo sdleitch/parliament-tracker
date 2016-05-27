174e98aeffbff046b068da4ac651001202837a78
Scott Leitch <s.d.leitch@gmail.com>
2016-05-27 12:44:54 -0400

    refactored bill/rm "self" calls, changed bills to bills_hash, added handle_asynchronously to :get_vote_tallies and :create_bills

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,20 +2,20 @@ class Bill < ActiveRecord::Base
   belongs_to :member
   has_many :vote_tallies, dependent: :destroy
 
-  # class methods for Bill
+  ### START OF CLASS METHODS ###
   class << self
 
     # Download bills xml, convert to Hash
     def scrape_bills
       @@bills_xml = open("http://www.parl.gc.ca/LEGISInfo/Home.aspx?language=E&ParliamentSession=42-1&Mode=1&download=xml").read
-      @@bills = Hash.from_xml(@@bills_xml)['Bills']['Bill'] # Scraped Array of bills
-      return @@bills
+      bills_hash = Hash.from_xml(@@bills_xml)['Bills']['Bill'] # Scraped Array of bills
+      return bills_hash
     end
 
     def create_bills
-      bills = self.scrape_bills
+      bills_hash = scrape_bills
 
-      bills.each do |bill|
+      bills_hash.each do |bill|
         new_bill = Bill.find_or_create_by(parliament_number: bill["id"])
 
         new_bill.prefix = bill["BillNumber"]["prefix"] if new_bill.prefix == nil
@@ -35,30 +35,32 @@ class Bill < ActiveRecord::Base
         new_bill.get_vote_tallies
         new_bill.save!
       end
+
     end
-    handle_asynchronously :create_bills, :run_at => 15.seconds.from_now
+    handle_asynchronously :create_bills
 
   end
 
-  def bill_number
-    return "#{self.prefix}-#{self.number}"
-  end
+  ### END OF CLASS METHODS###
+  ### START OF INSTANCE METHODS ###
 
-  # TEST THIS ALL
-  # Doesn't account for MPs no longer serving
   def get_vote_tallies
     base_uri = "http://www.parl.gc.ca"
-    votes_uri = base_uri + "/LEGISInfo/BillDetails.aspx?Language=E&Mode=1&billId=#{self.parliament_number}&View=5"
+    votes_uri = base_uri + "/LEGISInfo/BillDetails.aspx?Language=E&Mode=1&billId=#{parliament_number}&View=5"
     votes_page = Nokogiri::HTML(open(votes_uri))
-    # votes = votes_page.css(".VoteLink")
     vote_links = votes_page.css(".VoteLink").select { |vote| vote.attr('href') }
 
-    if vote_links.length > self.vote_tallies.length
+    if vote_links.length > vote_tallies.length
       vote_links.each do |vote_link|
         new_tally = VoteTally.create_vote_tally(base_uri + vote_link.attr('href'))
-        self.vote_tallies << new_tally
+        vote_tallies << new_tally
       end
     end
+  end
+  handle_asynchronously :get_vote_tallies
+
+  def bill_number
+    return "#{prefix}-#{number}"
   end
 
 end
