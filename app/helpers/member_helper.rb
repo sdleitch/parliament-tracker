14f0c7cd6b68849e9e3f808ea7dda7b1d730b8a3
Scott Leitch <s.d.leitch@gmail.com>
2016-06-02 13:45:01 -0400

    beginning styling on members/vote tables, also added bill and vote_tally routes

--- app/helpers/member_helper.rb
+++ app/helpers/member_helper.rb
@@ -1,7 +1,7 @@
 module MemberHelper
 
-  def sort_votes_by_date
-    sorted_votes = Vote.where(member_id: @member.id).limit(5).joins(:vote_tally).order('vote_tallies.date DESC')
+  def sort_votes_by_date(limit=nil)
+    sorted_votes = Vote.where(member_id: @member.id).limit(limit).joins(:vote_tally).order('vote_tallies.date DESC')
   end
 
   def name_with_honorific
