d00c11497c0e6a0e178019c12970a91ca35703f3
Scott Leitch <s.d.leitch@gmail.com>
2016-06-07 10:54:07 -0400

    i dunno

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module ParliamentTracker
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    # config.active_job.queue_adapter = :delayed_job
+    config.active_job.queue_adapter = :delayed_job
 
     config.paperclip_defaults = {
       storage: :s3,
