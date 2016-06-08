d072a8c6038bf67a6296871df67e2869f7770aa8
Scott Leitch <s.d.leitch@gmail.com>
2016-06-08 01:25:41 -0400

    added very basic application#index view. method routed to root.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    @bills = Bill.where('last_event_date >= ?', 1.week.ago.to_date).order('bills.last_event_date DESC')
+    @vote_tallies = VoteTally.where('date >= ?', 1.week.ago.to_date).order('vote_tallies.date DESC')
+  end
 end
