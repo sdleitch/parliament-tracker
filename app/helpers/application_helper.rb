cb06fa9226f64fbc125e6631b02bd934dc3c411f
Scott Leitch <s.d.leitch@gmail.com>
2016-06-19 10:51:51 -0400

    added page_title helper, adding bill summaries to views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def page_title
+    case
+    when @member
+      @member.fullname
+    when @bill
+      @bill.bill_number
+    when @vote_tally
+      "Vote No. " + @vote_tally.vote_number.to_s
+    else
+      "Parliament Tracker"
+    end
+  end
+
 end
