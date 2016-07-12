4b0fedb30757494370c0472ae410c33863a8892a
Scott Leitch <s.d.leitch@gmail.com>
2016-07-12 00:59:50 -0400

    added sort by vote_number to member helper method

--- app/helpers/member_helper.rb
+++ app/helpers/member_helper.rb
@@ -3,9 +3,9 @@ module MemberHelper
   def name_with_honorific
     @member.honorific ? "The #{@member.honorific} #{@member.fullname}" : @member.fullname
   end
-  
+
   def sort_votes_by_date(limit=nil)
-    sorted_votes = Vote.where(member_id: @member.id).limit(limit).joins(:vote_tally).order('vote_tallies.date DESC')
+    sorted_votes = Vote.where(member_id: @member.id).limit(limit).joins(:vote_tally).order('vote_tallies.date DESC, vote_tallies.vote_number DESC')
   end
 
   def sort_bills_by_date(limit=nil)
