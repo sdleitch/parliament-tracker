677402f928ab54deeb20fb4a904528df6a44e77b
Scott Leitch <s.d.leitch@gmail.com>
2016-08-08 21:54:30 -0400

    added party _votes view, altered vote_tally sort helper emthods to be used in new _votes, stylesheet changes'

--- app/helpers/bill_helper.rb
+++ app/helpers/bill_helper.rb
@@ -1,7 +1,13 @@
 module BillHelper
 
-  def sort_votes_tallies_by_date(limit=nil)
-    sorted_votes = VoteTally.where(bill_id: @bill.id).limit(limit).joins(:bill).order(date: :desc, vote_number: :desc)
+  def sort_vote_tallies_by_date(limit=nil)
+    if @bill
+      sorted_votes = VoteTally.where(bill_id: @bill.id).limit(limit).joins(:bill).order(date: :desc, vote_number: :desc)
+    else
+      sorted_votes = VoteTally.all.limit(limit).order(date: :desc, vote_number: :desc)
+    end
+    # raise 'hell'
+    return sorted_votes
   end
 
 end
