c1d06946a2cf0d61e3af276a042730803a0d434f
Scott Leitch <s.d.leitch@gmail.com>
2016-08-13 02:06:30 -0400

    moved helper method

--- app/helpers/vote_tally_helper.rb
+++ app/helpers/vote_tally_helper.rb
@@ -1,5 +1,14 @@
 module VoteTallyHelper
 
+  def sort_vote_tallies_by_date(limit=nil)
+    if @bill
+      sorted_votes = VoteTally.where(bill_id: @bill.id).limit(limit).joins(:bill).order(date: :desc, vote_number: :desc)
+    else
+      sorted_votes = VoteTally.all.limit(limit).order(date: :desc, vote_number: :desc)
+    end
+    return sorted_votes
+  end
+
   def result(tally)
     if tally.agreed_to == nil
       return 'tied'
