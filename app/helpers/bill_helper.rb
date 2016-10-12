ad79e59c4b71d8b2f316ff5e06da872770dc5d1f
Scott Leitch <s.d.leitch@gmail.com>
2016-10-12 13:38:40 -0400

    added summary helper to insert Act names into Bill summaries

--- app/helpers/bill_helper.rb
+++ app/helpers/bill_helper.rb
@@ -1,2 +1,9 @@
 module BillHelper
+
+  def summary_helper(bill)
+    if bill.summary.include?("  ") && /the [A-Z].+Act/.match(bill.title_long)
+      return bill.summary.gsub("  ", " " + /the [A-Z].+Act/.match(bill.title_long).to_s[4..-1] + " ")
+    end
+  end
+
 end
