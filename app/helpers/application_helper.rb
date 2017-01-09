ca396d7f60fa45161fb59fe09506f8dd15d82baa
Scott Leitch <s.d.leitch@gmail.com>
2017-01-09 16:51:22 -0500

    very basic map add to MP page. Finally.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  
+
   # Returns a <title> for each page
   def page_title
     case
@@ -12,6 +12,18 @@ module ApplicationHelper
     else
       "Parliament Tracker"
     end
+  end
+
+  # Builds a geoJSON string to pass into browser as
+  # geo attribute on the map div. Skips "null" values
+  def all_party_geo
+    geo = "{ \"type\": \"FeatureCollection\", \"features\": ["
+    ElectoralDistrict.all.each do |district|
+      unless district.geo == "null"
+        geo += district.geo + ", "
+      end
+    end
+    geo = geo[0..-3] + "] }"
   end
 
 end
